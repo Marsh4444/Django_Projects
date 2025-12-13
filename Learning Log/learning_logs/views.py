63e771f7464d8c38100423c40399606a7192a98b
Marsh <jamiakoko@gmail.com>
2025-12-13 17:31:56 +0100

    rounded up for rthe week with the users registration and make different users secure there account

--- Learning Log/learning_logs/views.py
+++ Learning Log/learning_logs/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from .models import Topic, Entry
 from .forms import TopicForm , EntryForm
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 def home(request):
@@ -13,7 +14,7 @@ def home(request):
 def topics(request):
     """The page that shows all topics."""
 
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}                                
     return render(request, 'learning_logs/topics.html', context)
 
@@ -21,6 +22,10 @@ def topics(request):
 def topic(request, topic_id):
     """page that Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
+    
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -35,7 +40,9 @@ def new_topic(request):
         #POST data submitted; process data.
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     
     context = {'form': form}
@@ -67,6 +74,9 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    #check whether the owner of the topic matches the currently logged in user
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
